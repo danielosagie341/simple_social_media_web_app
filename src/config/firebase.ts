5d4711fd1c142381f963ffb143c46c02004a859e
daniel osagie <danielosagie341@gmail.com>
2024-08-22 16:27:27 +0100

    third commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,25 +1,22 @@
-// Import the functions you need from the SDKs you need
+// firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-   apiKey: "AIzaSyD1T_nBM9fCDg4fL6Tq8txiP-p9N8bgWaw",
-  authDomain: "simple-socials-two.firebaseapp.com",
-  projectId: "simple-socials-two",
-  storageBucket: "simple-socials-two.appspot.com",
-  messagingSenderId: "937752270697",
-  appId: "1:937752270697:web:68c19c43d69a5f30334ff8",
-  measurementId: "G-T1ZKEVVGFQ" 
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
